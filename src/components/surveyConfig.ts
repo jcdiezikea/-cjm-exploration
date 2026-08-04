8903e7cd6c95d23e37adb188d66897a1354274fa
Juan Carlos Diez Rodriguez <jc@MacBook-Pro-de-Juan.local>
2026-08-04 11:26:58 +0200

    config: wire JSONBin credentials

--- src/components/surveyConfig.ts
+++ src/components/surveyConfig.ts
@@ -1,4 +1,4 @@
 // Replace both TODOs after creating a JSONBin.io account → Master Key + a Collection
-export const JSONBIN_KEY        = 'TODO_API_KEY'
-export const JSONBIN_COLLECTION = 'TODO_COLLECTION_ID'
+export const JSONBIN_KEY        = '$2a$10$9u3fZe8TVAJahD9EJtOWuOGJ8III6vRLrZU.PpNQ6DS7zb5Cdpdaa'
+export const JSONBIN_COLLECTION = '6a71b037da38895dfeb782ac'
 export const ADMIN_PASSWORD     = 'salgamosdepobres'
